d966185a30c0920ba8b62c13499768962f7dac28
Aaron Cimolini <aaron@cimolini.com>
2014-10-26 13:07:03 -0400

    Fixes createadmin.py issue where superuser and staff status weren't applied.

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -2,7 +2,5 @@
 
 from django.contrib.auth.models import User
 if User.objects.count() == 0:
-    admin = User.objects.create_user('admin', 'aaron@cimolini.com', 'admin')
-    admin.is_superuser = True
-    admin.is_staff = True
+    admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
     admin.save()
